44c86af51d12a1b960874aa19dd6824e963fd44f
andrewpham74@gmail.com <tiff.t.pham@gmail.com>
2014-11-14 10:16:12 -0500

    implemented Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # require for heroku
+  config.action_mailer.default_url_options = { :host => 'omr-andrewpham.herokuapp.com/' }
+
 end
